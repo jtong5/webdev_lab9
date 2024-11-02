050eedc7ffc6131a2eed55b3ad324a55b0124e2c
jtong5 <jtongplus@gmail.com>
2024-11-02 18:07:02 -0500

    add js code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+  var button = document.getElementById('entrybutton'); // var a = document.getElementById("elementid); 
 
+  function myEventFunction(){
+    var popup = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = popup.value; // sets what textoutput says because of innerHTML
+    alert("Jalan Tong: " + popup.value); // pop up
+  }
 
-
-
-
-
-
+  button.addEventListener('click', myEventFunction);
+  }
+  
 window.addEventListener('load', init);
